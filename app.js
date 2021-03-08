f024ff2a16dc1894ffa4e34c1cdab4155441e87e
Andrew Schroepfer <mendrelljack@gmail.com>
2021-03-07 16:01:13 -0800

    test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.nav-links li')
 
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');//grabs the nav-links and applys the styles listed in nav-active
+    });
+}
+
+navSlide(); //dont forget to invoke the function
